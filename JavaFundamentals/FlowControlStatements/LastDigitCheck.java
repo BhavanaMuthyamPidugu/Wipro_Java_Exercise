7bd8d9583393945d2083719e40efd74db6ed5406
BhavanaMuthyamPidugu <bhavana.pidugu@sasi.ac.in>
2025-07-28 16:27:18 +0530

    Code push to FlowControlStatements Folder

---
+++ JavaFundamentals/FlowControlStatements/LastDigitCheck.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class LastDigitCheck{
+    public static void main(String args[]){
+        System.out.print("Enter Two numbers to check whether they have same last digit : ");
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int r1=n1%10;
+        int r2=n2%10;
+        if(r1==r2){
+            System.out.println("Same i.e. "+r1);
+        }else{
+            System.out.println("Different i.e. "+r1+" and "+r2);
+        }
+    }
+}
---
+++ JavaFundamentals/FlowControlStatements/NumberSign.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class NumberSign{
+    public static void main(String args[]){
+        System.out.print("Enter number to check sign of it : ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }else if(n==0){
+            System.out.println("Zero");
+        }else{
+            System.out.println("Negative");
+        }
+    }
+}
